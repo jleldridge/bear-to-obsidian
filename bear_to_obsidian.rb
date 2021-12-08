10209dc34d23def36142a3aba1c68e65021170f0
Luke Eldridge <jleldridge27@gmail.com>
2021-12-07 20:16:57 -0800

    Implement image linking and file copying.

--- bear_to_obsidian.rb
+++ bear_to_obsidian.rb
@@ -1,8 +1,12 @@
 require 'sqlite3'
 require 'date'
+require 'fileutils'
 
 data_root = '/Users/luke/library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data'
 db = SQLite3::Database.new "#{data_root}/database.sqlite"
+zettel_directory = '/Users/luke/Library/Mobile Documents/iCloud~md~obsidian/Documents/bear-to-zettel'
+Dir.mkdir "#{zettel_directory}/images" unless File.directory?("#{zettel_directory}/images")
+
 
 # Bear stores it's timestamps using unixepoch format with it's own start time.
 # The offset value I've determined (978307200) seems to be Jan 1, 2001 12:00:00 AM
@@ -10,22 +14,41 @@ notes_query = <<-SQL
   select datetime(ZCREATIONDATE + 978307200, 'unixepoch'), ZTITLE, ZTEXT from ZSFNOTE
   where ZTRASHED = 0
     and ZPERMANENTLYDELETED = 0
-    and ZTEXT LIKE '%project-slime%'
 SQL
 
+used_file_names = {}
+note_links = {}
+
 db.execute(notes_query) do |row|
-  file_name = "#{DateTime.parse(row[0]).strftime('%Y%m%d%H%M')} #{row[1]}"
   text = row[2]
-#   puts file_name
-#   puts text
 
-  # TODO: search each file for image references and copy the images into the zettel directory
-  images = text.match(/\[image:.*\]/)
+  images = text.scan(/\[image:.*\]/)
   images.each do |image|
+    # copy image file to zettel directory
+    rel_image_path = "#{image[7...image.length-1]}"
+    new_image_name = rel_image_path.gsub('/', '_')
+    FileUtils.cp("#{data_root}/Local Files/Note Images/#{rel_image_path}", "#{zettel_directory}/images/#{new_image_name}")
 
-    # text.gsub!(image, )
+    # sub in markdown style link, with image in images directory
+    text = text.gsub!(image, "![image](images/#{new_image_name})")
   end
 
+  new_note_name = if row[1].empty? 
+                    "#{DateTime.parse(row[0]).strftime('%Y%m%d%H%M')}.md"
+                  else
+                    "#{DateTime.parse(row[0]).strftime('%Y%m%d%H%M')} #{row[1]}.md"
+                  end
 
-  # TODO?: search each file for links to other notes and modify them to properly link the note
+  if !used_file_names[new_note_name].nil?
+    suffix = used_file_names[new_note_name]
+    used_file_names[new_note_name] += 1
+    new_note_name = "#{new_note_name[0...-3]}-#{suffix}.md"
+  else
+    used_file_names[new_note_name] = 1
+  end
+
+  File.open("#{zettel_directory}/#{new_note_name}", 'w') { |f| f.write(text) }
+  links[row[0]] = new_note_name
 end
+
+
